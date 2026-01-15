e29905b701631066d83212eef6c5dc6dd2826aa5
Sandro de Oyá <caminhoscompadilha@gmail.com>
2026-01-15 23:01:02 +0000

    npm run build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 
 import type {Metadata} from 'next';
-import { Literata, Space_Grotesk } from 'next/font/google';
 import './globals.css';
 
 export const metadata: Metadata = {
@@ -8,30 +7,14 @@ export const metadata: Metadata = {
   description: 'Desvende os segredos do seu mapa celestial.',
 };
 
-const literata = Literata({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-literata',
-  weight: ['400', '700'],
-  style: ['normal', 'italic'],
-});
-
-const spaceGrotesk = Space_Grotesk({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-space-grotesk',
-  weight: ['400', '700'],
-});
-
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-BR" className={`${literata.variable} ${spaceGrotesk.variable} dark`} translate="no">
-      <body className="font-body antialiased">
+    <html lang="pt-BR" translate="no">
+      <body>
           {children}
       </body>
     </html>
